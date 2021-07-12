4c8f1f280adfa2ffa8f5e1cca906ac6e0ffd1bb5
Ramon Lima <lima.ramon13@gmail.com>
2021-07-11 23:35:56 -0300

    Estilizando Status Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    </>
+  );
+};
 
 export default App;
