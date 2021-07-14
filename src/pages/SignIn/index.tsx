f99312e27fba69c16e23d7be36a24e07b8983cd9
Ramon Lima <lima.ramon13@gmail.com>
2021-07-14 19:16:59 -0300

    estilizando botão de criar conta

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title, ForgotPassword, ForgotPasswordText } from './style';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccount,
+  CreateAccountText,
+} from './style';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <Container>
+        <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+        <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button
+        <Button
+          onPress={() => {
+            console.log('click');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword
+          onPress={() => {
+            console.log('esqueci minha senha');
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccount
         onPress={() => {
-          console.log('click');
+          console.log('criar conta');
         }}
       >
-        Entrar
-      </Button>
+        <Icon name="log-in" size={20} color="#ff9000" />
 
-      <ForgotPassword onPress={() => {}}>
-        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-      </ForgotPassword>
-    </Container>
+        <CreateAccountText>CriarConta</CreateAccountText>
+      </CreateAccount>
+    </>
   );
 };
 
--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -23,4 +24,26 @@ export const ForgotPasswordText = styled.Text`
   font-family: 'RobotoSlab-Regular';
   color: #f4ede8;
   font-size: 16px;
+`;
+
+export const CreateAccount = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
