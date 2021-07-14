884ad6d8cae2d622537f0a302f0a8be504fd8e53
Ramon Lima <lima.ramon13@gmail.com>
2021-07-14 19:26:22 -0300

    Adicionando ScrollView e fechamento do teclado ao apertar fora do input

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
@@ -23,42 +29,47 @@ const SignIn: React.FC = () => {
       style={{ flex: 1 }}
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Container>
-        <Image source={logoImg} />
-
-        <View>
-          <Title>Faça seu logon</Title>
-        </View>
-
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" icon="lock" placeholder="Senha" />
-
-        <Button
-          onPress={() => {
-            console.log('click');
-          }}
-        >
-          Entrar
-        </Button>
-
-        <ForgotPassword
-          onPress={() => {
-            console.log('esqueci minha senha');
-          }}
-        >
-          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-        </ForgotPassword>
-      </Container>
-
-      <CreateAccount
-        onPress={() => {
-          console.log('criar conta');
-        }}
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
       >
-        <Icon name="log-in" size={20} color="#ff9000" />
+        <Container>
+          <Image source={logoImg} />
 
-        <CreateAccountText>CriarConta</CreateAccountText>
-      </CreateAccount>
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              console.log('click');
+            }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword
+            onPress={() => {
+              console.log('esqueci minha senha');
+            }}
+          >
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+
+        <CreateAccount
+          onPress={() => {
+            console.log('criar conta');
+          }}
+        >
+          <Icon name="log-in" size={20} color="#ff9000" />
+
+          <CreateAccountText>CriarConta</CreateAccountText>
+        </CreateAccount>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
